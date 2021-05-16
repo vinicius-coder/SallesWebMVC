ffbab9d7dda9d21e8a9a7b64c25b863ef231d843
Avonts <vinialves022@gmail.com>
2021-05-16 19:17:49 -0300

    Add Seeding service

--- SallesWebMVC/Startup.cs
+++ SallesWebMVC/Startup.cs
@@ -39,14 +39,18 @@ namespace SallesWebMVC
             services.AddDbContext<SallesWebMVCContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SallesWebMVCContext"), builder =>
                         builder.MigrationsAssembly("SallesWebMVC")));
+
+            services.AddScoped<SeedingService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
