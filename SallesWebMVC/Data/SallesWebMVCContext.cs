c46ce6d27bbf5730f240fd9fd68568a2e168168a
Avonts <vinialves022@gmail.com>
2021-05-16 18:54:25 -0300

    Add others entities and second migration

--- SallesWebMVC/Data/SallesWebMVCContext.cs
+++ SallesWebMVC/Data/SallesWebMVCContext.cs
@@ -14,6 +14,8 @@ namespace SallesWebMVC.Data
         {
         }
 
-        public DbSet<SallesWebMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ SallesWebMVC/Models/Seller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SallesWebMVC.Models
+{
+    public class Seller
+    {
+
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public DateTime BithDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime bithDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BithDate = bithDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= inicial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+
+    }
+}
