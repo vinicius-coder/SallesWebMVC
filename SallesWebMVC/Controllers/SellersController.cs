fe62124ce67c519284d746fd78183c5c7bbf995d
Avonts <vinialves022@gmail.com>
2021-05-17 12:25:55 -0300

    Add Seller details and eager loading

--- SallesWebMVC/Controllers/SellersController.cs
+++ SallesWebMVC/Controllers/SellersController.cs
@@ -62,5 +62,18 @@ namespace SallesWebMVC.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null) { return NotFound(); }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
--- SallesWebMVC/Services/SellerService.cs
+++ SallesWebMVC/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SallesWebMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SallesWebMVC.Data;
 using SallesWebMVC.Models;
 using System;
 using System.Collections.Generic;
@@ -30,7 +31,7 @@ namespace SallesWebMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
