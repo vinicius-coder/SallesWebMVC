bee71d92b4c8ad18e6db90b0557ea80b2dec297a
Avonts <vinialves022@gmail.com>
2021-05-18 18:46:07 -0300

    Add Validation

--- SallesWebMVC/Models/Seller.cs
+++ SallesWebMVC/Models/Seller.cs
@@ -10,18 +10,27 @@ namespace SallesWebMVC.Models
     {
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size shound be between {2} and {1}")]
         public String Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public String Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name="Bith Date")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
