7bcb7acb30da88a0b3eb64989d1f5406e8a4289f
Avonts <vinialves022@gmail.com>
2021-05-17 22:16:29 -0300

    Add app locale, number and date formatting

--- SallesWebMVC/Startup.cs
+++ SallesWebMVC/Startup.cs
@@ -12,6 +12,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SallesWebMVC.Data;
 using SallesWebMVC.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace SallesWebMVC
 {
@@ -50,6 +52,18 @@ namespace SallesWebMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+
+            var enUS = new CultureInfo("en-US");
+
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
